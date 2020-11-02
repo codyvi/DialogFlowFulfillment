cd621f634ce5b068d35b19ab21188205d2ee6fe5
David <davidcantumartinez@gmail.com>
2020-11-01 20:16:09 -0600

    Staring FAQ intent

---
+++ index.js
@@ -0,0 +1,40 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function faqsHandler(agent){
+    const pregunta = agent.parameters.Preguntas;
+    let response = 'Perdón esa pregunta no se encuentra';
+    if(pregunta == '¿Que tipo de doctores hay?'){
+      response = 'Hay doctores para consulta general y oncologos';
+    }
+    agent.add(response);
+  }
+  
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('PreguntasFrecuentes', faqsHandler);
+  agent.handleRequest(intentMap);
+});
